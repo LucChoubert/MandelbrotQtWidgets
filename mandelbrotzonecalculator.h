c281b40a1b77beb8db30e46d7b772f25c5b026b6
Luc CHOUBERT <lchoubert@amadeus.com>
2017-08-31 10:39:14 +0200

    Implementation of Mandelbrot Calculation

--- mandelbrotzonecalculator.h
+++ mandelbrotzonecalculator.h
@@ -1,12 +1,14 @@
 #ifndef MANDELBROTZONECALCULATOR_H
 #define MANDELBROTZONECALCULATOR_H
 
+#include <QPair>
 
 class MandelbrotZoneCalculator
 {
 public:
     MandelbrotZoneCalculator(float ix_min, float ix_max, float iy_min, float iy_max, int in_pixel, int iiter_max);
-    void compute();
+    void computeZone();
+    QPair<bool, int> compute(float c_x, float c_y);
 
 private:
     float x_min;
